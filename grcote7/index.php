dd98c5fa17cb1e318d2103b179359bedbcde5e36
Lionel COTE <grcote7@gmail.com>
2022-09-26 13:43:46 +0200

    Add icons

--- grcote7/index.php
+++ grcote7/index.php
@@ -1,7 +1,40 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-/**
- * (ɔ) Online FORMAPRO - GrCOTE7 -2022.
- */
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=
+  , initial-scale=1.0">
+  <title>Document</title>
+  <style>
+  body {
+    background-color: beige;
+  }
+  </style>
+  <script src="./config/browser-sync/sync_gc7.js"></script>
+</head>
 
-echo 'Bonjour @ tou(te)s ici :-))!';
+<body>
+  <h1>
+    <?php
+  /**
+   * (ɔ) Online FORMAPRO - GrCOTE7 -2022.
+   */
+  
+  // echo 'Bonjour @ tou(te)s ici :-))!';
+  ?>
+  </h1>
+
+  <p>
+    <img src="./cv_portfolio/assets/icons/front/tailwindcss-logotype/tailwindcss-logotype/tailwindcss-logotype.svg"
+      alt="">
+    <img
+      src="./cv_portfolio/assets/icons/front/tailwindcss-logotype/tailwindcss-logotype/tailwindcss-logotype-white.svg"
+      alt="">
+
+  </p>
+
+</body>
+
+</html><?php
