5eff490ec76064d2e16578b5c18a5e7f91def6b4
Lionel COTE <grcote7@gmail.com>
2022-09-29 21:51:31 +0200

    Fix logo TailWind

--- grcote7/index.php
+++ grcote7/index.php
@@ -2,38 +2,33 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=
   , initial-scale=1.0">
-  <title>Document</title>
-  <style>
-  body {
-    background-color: beige;
-  }
-  </style>
-  <script src="./config/browser-sync/sync_gc7.js"></script>
+	<title>Document</title>
+	<style>
+		body {
+			background-color: beige;
+		}
+	</style>
+	<script src="./config/browser-sync/sync_gc7.js"></script>
 </head>
 
 <body>
-  <h1>
-    <?php
+	<h1>
+		<?php
   /**
    * (ɔ) Online FORMAPRO - GrCOTE7 -2022.
    */
-  
+
   // echo 'Bonjour @ tou(te)s ici :-))!';
-  ?>
-  </h1>
+		?>
+	</h1>
 
-  <p>
-    <img src="./cv_portfolio/assets/icons/front/tailwindcss-logotype/tailwindcss-logotype/tailwindcss-logotype.svg"
-      alt="">
-    <img
-      src="./cv_portfolio/assets/icons/front/tailwindcss-logotype/tailwindcss-logotype/tailwindcss-logotype-white.svg"
-      alt="">
-
-  </p>
+	<p>
+		<img src="./cv_portfolio/tailwindcss-logotype.svg" alt="">
+	</p>
 
 </body>
 
